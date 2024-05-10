a0de8efb04c801fd70aaf0df8189a2e0b285c2d3
Leonardo Theodoro <leonardo@recruitrobin.com>
2024-05-10 08:23:17 -0300

    add permissions with conditionals

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -13,9 +13,10 @@ export const permissions: Record<Role, PermissionByRole> = {
   ADMIN: (_, { can }) => {
     can('manage', 'all')
   },
-  MEMBER: (_, { can }) => {
-    // can('invite', 'User')
-    can('manage', 'Project')
+  MEMBER: (user, { can }) => {
+    can(['create', 'get'], 'Project')
+    can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } })
+    can(['update', 'delete'], 'Organization', { ownerId: { $eq: user.id } })
   },
   BILLING: (_, { can }) => {
     // can('invite', 'User')
