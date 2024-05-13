a28bf6edf5f41627cef50aa3a7e701bf6e657481
Leonardo Theodoro <leonardo@recruitrobin.com>
2024-05-13 09:14:20 -0300

    setup fastify

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER' })
-
-const userCanDeleteUser = ability.can('delete', 'User')
-
-console.log(userCanDeleteUser)
