bba8b56e94b5fad765aa6c98a2f8f7415cd5f6bb
Leonardo Theodoro <simplespinheiro@gmail.com>
2024-05-21 20:52:04 -0300

    add revoke invite route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,8 @@ import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
 import { getInvites } from './routes/invites/get-invites'
+import { rejectInvite } from './routes/invites/reject-invite'
+import { revokeInvite } from './routes/invites/revoke-invite'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -104,6 +106,8 @@ app.register(createInvite)
 app.register(getInvites)
 app.register(getInvite)
 app.register(acceptInvite)
+app.register(rejectInvite)
+app.register(revokeInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running...')
