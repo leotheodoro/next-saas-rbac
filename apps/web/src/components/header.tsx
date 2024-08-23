74eaf73c1a96dab8ff0ecb9573dfda4db9075cb2
Leonardo Theodoro <leonardo@recruitrobin.com>
2024-08-23 09:07:26 -0300

    add new organization switcher component

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,7 +1,9 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import robinIcon from '@/assets/robin-icon.svg'
 
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
@@ -9,6 +11,10 @@ export function Header() {
     <header className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={robinIcon} className="size-6 dark:invert" alt="Robin" />
+
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
