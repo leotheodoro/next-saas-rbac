d677df63874a088f537f471d1f302dd6f1d46bb8
Leonardo Theodoro <leonardo@recruitrobin.com>
2024-09-16 16:57:48 -0300

    add billing table

--- apps/web/src/app/(app)/org/[slug]/settings/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/settings/page.tsx
@@ -9,6 +9,7 @@ import {
 import { getOrganization } from '@/http/get-organization'
 
 import { OrganizationForm } from '../../organization-form'
+import { Billing } from './billing'
 import { ShutdownOrganizationButton } from './shutdown-organization-button'
 
 export default async function Settings() {
@@ -55,7 +56,7 @@ export default async function Settings() {
           </Card>
         )}
 
-        {canGetBilling && <div>billing</div>}
+        {canGetBilling && <Billing />}
       </div>
     </div>
   )
