eceb828111ccdbfb715ee87747b43f64be6043d8
Leonardo Theodoro <leonardo@recruitrobin.com>
2024-09-17 15:29:12 -0300

    add project list structure

---
+++ apps/web/src/app/(app)/org/[slug]/(projects)/projects-list.tsx
@@ -0,0 +1,45 @@
+import { ArrowRight } from 'lucide-react'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+export function ProjectsList() {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Projeto 01</CardTitle>
+          <CardDescription className="line-clamp-2 leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam
+            repellat ab nulla voluptatem deleniti placeat eaque nostrum impedit,
+            corrupti repudiandae, veniam laudantium praesentium commodi.
+            Numquam, tempore. Excepturi dolorum nisi sint?
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="flex items-center gap-1.5">
+          <Avatar className="size-4">
+            <AvatarImage src="https://githu.com/leotheodoro.png" />
+            <AvatarFallback />
+          </Avatar>
+
+          <span className="text-xs text-muted-foreground">
+            Created by{' '}
+            <span className="font-medium text-foreground">Leo theodoro</span> a
+            day ago
+          </span>
+
+          <Button size="xs" variant="outline" className="ml-auto">
+            View <ArrowRight className="ml-2 size-3" />
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function Projects() {
-  return <div className="space-y-4">orgs</div>
-}
